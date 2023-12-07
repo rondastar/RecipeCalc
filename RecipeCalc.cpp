76d5f49d3917573808d6e6b8fbbfec92107348ad
rondastar <116147793+rondastar@users.noreply.github.com>
2023-12-06 19:24:40 -0800

    Removed multiplyAndPrintQuantity function
    split function into separate functions to multiply quantity, and another to print quantity

--- RecipeCalc.cpp
+++ RecipeCalc.cpp
@@ -5,44 +5,6 @@
 #include <vector>
 #include <cmath>
 
-void multiplyAndPrintQuantity(Quantity qty, int multiplier) {
-    int productWholeNumber{ 0 };        // default 0
-    int productNumerator{ 0 };          // default 0
-    int productDenominator{ -1 };       // default -1
-    if (qty.getWholeNumber() != 0) {    // if qty has whole number, multiply it
-        productWholeNumber = (qty.getWholeNumber() * multiplier);
-        if (qty.getNumerator() == 0) {  // if there is no fraction, print whole number and return.
-            std::cout << productWholeNumber << std::endl;
-            return;
-        }
-    }
-    if (qty.getNumerator() != 0) {   // if qty has fraction
-        productNumerator = (qty.getNumerator() * multiplier);  // multiply it
-        productDenominator = qty.getDenominator();           // denominator remains the same
-
-        // reduce fraction if possible
-        if (productNumerator >= productDenominator) {
-            productWholeNumber += (productNumerator / productDenominator);
-            productNumerator = (productNumerator % productDenominator);
-            if (productNumerator == 0) {       // prints if reduces to whole number only
-                std::cout << productWholeNumber << std::endl;
-                return;
-            }
-        }
-        else if ((productNumerator % multiplier == 0) && (productDenominator % multiplier == 0)) {
-            productNumerator = (productNumerator / multiplier);
-            productDenominator = (productDenominator / multiplier);
-        }
-    }
-    if (productWholeNumber == 0) {          // prints fraction
-        std::cout << productNumerator << '/' << productDenominator << std::endl;
-        return;
-    }
-    else {                                  // prints mixed number
-        std::cout << productWholeNumber << ' ' << productNumerator << '/' << productDenominator << std::endl;
-        return;
-    }    
-}
 Quantity multiplyQuantity(Quantity qty, int multiplier) {
     int productWholeNumber{ 0 };        // default 0
     int productNumerator{ 0 };          // default 0
@@ -79,26 +41,33 @@ Quantity multiplyQuantity(Quantity qty, int multiplier) {
     }
 }
 
-void prepopulateQuantities() {
+void printQuantity(Quantity qty) {
+    // prints quantity (no endline)
+    if (qty.getWholeNumber() != 0) {
+        std::cout << qty.getWholeNumber() << ' ';
+    }
+    if (qty.getNumerator() != 0) {
+        std::cout << qty.getNumerator()
+            << "/" << qty.getDenominator() << ' ';
+    }
+}
+
+void prepopulateQuantities() {      // for testing functions
     Quantity mixedNum = Quantity::Quantity(1, 2, 3);  // 1 2/3  - test mixed number
     Quantity wholeNum = Quantity::Quantity(2);        // 2      - test small int
     Quantity fraction = Quantity::Quantity(1, 4);     // 1/4    - test fraction 
     Quantity wholeNum2 = Quantity::Quantity(500);     // 500    - test larger int
-    //multiplyQuantity(mixedNum, 10);   // works
-    //multiplyQuantity(wholeNum, 4);    // works
-    //multiplyQuantity(fraction, 2);    // works
-    //multiplyQuantity(wholeNum2, 2);   // NOTE: realized I need to set unspecified members to null or null pointers, or give them values
+
+    // testing functions using quantities
+    printQuantity(multiplyQuantity(mixedNum, 10));   // works
+    printQuantity(multiplyQuantity(wholeNum, 4));    // works
+    printQuantity(multiplyQuantity(fraction, 2));    // works
+    printQuantity(multiplyQuantity(wholeNum2, 2));   // NOTE: realized I need to initialize all variables
 }
 
 void printIngredientLine(IngredientLine il) {
     // prints ingredient line
-    if (il.getQuantity().getWholeNumber() != 0) {
-        std::cout << il.getQuantity().getWholeNumber() << ' ';
-    }
-    if (il.getQuantity().getNumerator() != 0) {
-        std::cout << il.getQuantity().getNumerator() 
-            << "/" << il.getQuantity().getDenominator() << ' ';
-    }
+    printQuantity(il.getQuantity());
     if (il.getUnit() != "0") {
 
         std::cout << il.getUnit() << ' ';
@@ -106,35 +75,72 @@ void printIngredientLine(IngredientLine il) {
     std::cout <<  il.getIngredient() << std::endl;
  }
 
-IngredientLine testIngredient() {
+IngredientLine testIngredient() {       // for testing functions
     Quantity testQty{ Quantity(3) };
     IngredientLine testIngredient{ IngredientLine(testQty, "cups", "flour") };
     return testIngredient;
 }
-//void getUserInput() {
-//    std::string rawInput;
-//    std::cout << "Enter a number, or numbers separated by a space, between 1 and 1000." << endl;
-//    while (std::getline(std::cin, rawInput, ' '))
-//    {
-//        IngredientLine ingredientLine = ingredientLine()
-//        bool isNum = true;
-//        for (int i = 0; i < rawInput.length() && isNum; ++i)
-//        {
-//            isNum = isdigit(rawInput[i]);
-//        }
-//
-//        if (isNum)
-//        {
-//            num = atoi(rawInput.c_str());
-//            ...//do your stuff
-//        }
-//        else
-//            std::cout << rawInput << " is not a number!" << endl;
-//    }
-//}
+
+void parseQuantityString() {
+    std::string line;
+    while (std::getline(std::cin, line))
+    {
+        std::istringstream iss(line);
+        std::string first_on_line;
+        std::string second_on_line;
+        std::string third_on_line;
+        if (iss >> first_on_line >> second_on_line >> third_on_line);
+            
+    }
+
+    //Quantity q = Quantity();
+    //    std::string rawInput;
+    //    std::cout << "Enter a whole number, mixed number, or fraction" << std::endl;
+    //    
+    //    while (std::getline(std::cin, rawInput, ' ')) {
+    //        int int1{ 0 };
+    //        int int2{ 0 };
+    //        int int3{ 0 };
+    //        //std::string str1{ "0" };
+    //        //std::string str2{ "0" };
+    //        //std::string str3{ "0" };
+    //        bool isNum{ true };
+
+    //        for (int i = 0; 1 < rawInput.length(); ++i) {
+    //            if (isNum) {
+    //                isNum = isdigit(rawInput[i]);
+    //                if (int1 == 0) {
+    //                    int1 = isdigit(rawInput[i]);
+    //                    break;
+    //                }
+    //                else if (int2 == 0) {
+    //                    int2 = isdigit(rawInput[i]);
+    //                    break;
+    //                }
+    //                else if (int3 == 0) {
+    //                    int3 = isdigit(rawInput[i]);
+    //                    break;
+    //                }
+    //                else {
+    //                    std::cout << "Please enter recipe line in format quantity"
+    //                        << "(whole number, fraction, or mixed number) unit ingredient." << std::endl;
+    //                    break;
+    //                }
+    //                //}
+    //                //else (is)
+    //                break;
+    //            }
+    //        } 
+
+    //        std::cout << "int1 is " << int1 << "\nint2 is" << int2 << "\nint3 is" << int3 << std::endl;
+    //    }
+
+}
+
 int main() {
-    prepopulateQuantities();
-    printIngredientLine(testIngredient());
+    //prepopulateQuantities();
+    //printIngredientLine(testIngredient());
+    parseQuantityString();
 
     return 0;
 }
